89e21e761132d58768c9eb65552335621467e1e3
gksdydrkfl <gksdydrkfl@gmail.com>
2023-05-19 10:06:50 +0900

    0518 과제 제출

---
+++ HomeWork/HomeWork_0518/HomeWork_0518.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+int StringCount(char* _Arr)
+{
+    if (nullptr == _Arr)
+    {
+        return 0;
+    }
+
+    int Count = 0;
+    while (_Arr[Count])
+    {
+        ++Count;
+    }
+    return Count;
+}
+
+void ChangeCh(char* _Arr, char _PrevCh, char _NextCh)
+{
+    if (nullptr == _Arr)
+    {
+        return;
+    }
+
+    int Count = 0;
+    while (_Arr[Count])
+    {
+        if (_Arr[Count] == _PrevCh)
+        {
+            _Arr[Count] = _NextCh;
+        }
+        ++Count;
+    }
+}
+
+int main()
+{
+    char Arr[100] = "312312321";
+
+    int Count = StringCount(Arr);
+
+    //char Arr[100] = "aaa bbb ccc";
+
+    ChangeCh(Arr, 'c', 'd');
+}
