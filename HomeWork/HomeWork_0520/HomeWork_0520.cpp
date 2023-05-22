4164634b5f3210d374ebc227239ac0b1ff2cca73
gksdydrkfl <gksdydrkfl@gmail.com>
2023-05-22 12:49:50 +0900

    0522 과제 중간

---
+++ HomeWork/HomeWork_0520/HomeWork_0520.cpp
@@ -0,0 +1,22 @@
+﻿// HomeWork_0520.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class Player
+{
+	int Hp;
+	int Att;
+};
+
+class Monster
+{
+	int Hp;
+	int Att;
+};
+
+int main()
+{
+
+
+}
